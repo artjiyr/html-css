07d9bf8c5decd2fd07b2c45e6a4907e9cdc05b95
artjiyr <106913775+artjiyr@users.noreply.github.com>
2026-01-11 14:58:04 -0300

    correções

--- GSAP/ex001/script.js
+++ GSAP/ex001/script.js
@@ -1,17 +1,16 @@
 gsap.fromTo(".quadrado", {
     x: -200,
     y:100,
-    opacity: 0,
     duration: 1
 },
 {
     x:100,
     y:-200,
     backgroundColor: "green",
-    duration: 5,
+    duration: 3,
     rotation: 0,
     stagger: .5,
-    ease: "elastic.out(1,0.3)",
+    ease: "back.out(1.7)",
     repeat: -1,
     yoyo: true,
     rotation: 360,
